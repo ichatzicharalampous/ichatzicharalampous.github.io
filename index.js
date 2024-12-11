5820db3bd5733d7ec52a9636767ce2e9693026e2
ichatzicharalampous <92908876+ichatzicharalampous@users.noreply.github.com>
2024-12-11 10:38:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const slides = document.querySelectorAll(".slides img")
+let slideIndex = 0;
+let intervalId = null;
+document.addEventListener("DOMContentLoaded",initSlide);
+
+function initSlide(){
+    slides[slideIndex].classList.add("displaySlide");
+    intervalId = setInterval(nextSlide,5000);
+    console.log(intervalId);
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+      slideIndex = 0;
+    }
+    else if(index < 0){
+      slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+      slide.classList.remove("displaySlide")
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    slideIndex--;
+    showSlide(slideIndex)
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
